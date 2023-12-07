5624c234e7d0894e1265cfe8eee6e7a2973b692c
EmilRox <129410389+EmilRox@users.noreply.github.com>
2023-12-07 23:59:05 +0200

    Add files via upload

---
+++ TEST-REZULT/Calculator_test.java
@@ -0,0 +1,18 @@
+public class Calculator_test {
+    
+    public static void main(String[] argsg) {
+        System.out.println("Rezultatul" +  " "  + Calculator.Run("2 +2"));
+        System.out.println("Rezultatul" +  " "  + Calculator.Run("2 + adc"));
+        System.out.println("Rezultatul" +  " "  + Calculator.Run("(2+3)*4-7 "));
+        System.out.println("Rezultatul" +  " "  + Calculator.Run("1/0"));
+        System.out.println("Rezultatul" +  " "  + Calculator.Run("((3/5)+4*4+6)"));
+        System.out.println("Rezultatul" +  " "  + Calculator.Run("(5+3*(9-8)+(2-5+6)/(4-6-8)-(6+9)"));
+        System.out.println("Rezultatul" +  " "  + Calculator.Run("2 +2 * 4"));
+
+    }
+    }
+
+      
+    
+    
+
